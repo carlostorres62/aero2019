252481b1c7a8bda9b0d276e2a867bd184c41a555
carlostorres <carloselitorres21@gmail.com>
2019-09-12 17:49:55 -0400

    created class, time needs some fix

--- first_draft.py
+++ first_draft.py
@@ -4,137 +4,124 @@ import time
 import datetime as dt
 from random import *
 
-master = tk.Tk()
-master.title("Aero Design")
-master.geometry("1000x425+125+50")
-master.resizable(0, 0)
 
+class Draft1:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Aero Design")
+        self.master.geometry("1000x425+125+50")
+        self.master.resizable(False, False)
 
-scrollbar = ttk.Scrollbar(master, orient=tk.VERTICAL)
-tree = ttk.Treeview(master, height=5, columns=5, yscrollcommand=scrollbar.set)
-start_time = time.time()
+        self.scrollbar = ttk.Scrollbar(master, orient=tk.VERTICAL)
+        self.tree = ttk.Treeview(master, height=5, columns=5, yscrollcommand=self.scrollbar.set)
+        self.start_time = time.time()
 
-style = ttk.Style()
-style.configure("Treeview.Heading", font=(None, 15))   # Configures the size of the headings
+        self.style = ttk.Style()
+        self.style.configure("Treeview.Heading", font=(None, 15))   # Configures the size of the headings
 
-height = 20         # Sets height size for tree
-tree["height"] = height
-tree["columns"] = ("one", "two", "three", "four", "five", "six")
-tree.heading("#0", text="Data", anchor=tk.CENTER)
-tree.heading("one", text="Time", anchor=tk.CENTER)
-tree.heading("two", text="Altitude", anchor=tk.CENTER)
-tree.heading("three", text="CDA", anchor=tk.CENTER)
-tree.heading("four", text="Water", anchor=tk.CENTER)
-tree.heading("five", text="Shelter", anchor=tk.CENTER)
-tree.heading("six", text="Weight", anchor=tk.CENTER)
+        self.height = 20         # Sets height size for tree
+        self.tree["height"] = self.height
+        self.tree["columns"] = ("one", "two", "three", "four", "five", "six")
+        self.tree.heading("#0", text="Data", anchor=tk.CENTER)
+        self.tree.heading("one", text="Time", anchor=tk.CENTER)
+        self.tree.heading("two", text="Altitude", anchor=tk.CENTER)
+        self.tree.heading("three", text="CDA", anchor=tk.CENTER)
+        self.tree.heading("four", text="Water", anchor=tk.CENTER)
+        self.tree.heading("five", text="Shelter", anchor=tk.CENTER)
+        self.tree.heading("six", text="Weight", anchor=tk.CENTER)
 
-# Carlos
-altP, tP, d1, d2 = tk.IntVar(), tk.StringVar(), tk.IntVar(), tk.IntVar()
+        # Carlos
+        self.altP, self.tP, self.d1, self.d2 = tk.IntVar(), tk.StringVar(), tk.IntVar(), tk.IntVar()
 
-data = ttk.Label(master, text="DATA:", font=("Helvetica", 25), style="R.TLabel")
-altitude = ttk.Label(master, text="Altitude:", font=("Helvetica", 20), style="B.TLabel")
-altitudeVar = ttk.Label(master, textvariable=altP, font=("Helvetica", 20))
-timeLabel = ttk.Label(master, text="Time:", font=("Helvetica", 20), style="B.TLabel")
-timeVar = ttk.Label(master, textvariable=tP, font=("Helvetica", 20))
-deploy1 = ttk.Label(master, text="Deploy 1: ", font=("Helvetica", 20), style="B.TLabel")
-deploy1Var = ttk.Label(master, textvariable=d1, font=("Helvetica", 20))
-deploy2 = ttk.Label(master, text="Deploy 2:", font=("Helvetica", 20), style="B.TLabel")
-deploy2Var = ttk.Label(master, textvariable=d2, font=("Helvetica", 20))
+        self.data = ttk.Label(master, text="DATA:", font=("Helvetica", 25), style="R.TLabel")
+        self.altitude = ttk.Label(master, text="Altitude:", font=("Helvetica", 20), style="B.TLabel")
+        self.altitudeVar = ttk.Label(master, textvariable=self.altP, font=("Helvetica", 20))
+        self.timeLabel = ttk.Label(master, text="Time:", font=("Helvetica", 20), style="B.TLabel")
+        self.timeVar = ttk.Label(master, textvariable=self.tP, font=("Helvetica", 20))
+        self.deploy1 = ttk.Label(master, text="Deploy 1: ", font=("Helvetica", 20), style="B.TLabel")
+        self.deploy1Var = ttk.Label(master, textvariable=self.d1, font=("Helvetica", 20))
+        self.deploy2 = ttk.Label(master, text="Deploy 2:", font=("Helvetica", 20), style="B.TLabel")
+        self.deploy2Var = ttk.Label(master, textvariable=self.d2, font=("Helvetica", 20))
 
-style.configure("B.TLabel", foreground="Blue")
-style.configure("R.TLabel", foreground="Red")
+        self.style.configure("B.TLabel", foreground="Blue")
+        self.style.configure("R.TLabel", foreground="Red")
 
-data.place(relx=3 / 4, rely=0.05)
-altitude.place(relx=3 / 4, rely=1 / 5)
-altitudeVar.place(relx=0.9, rely=1 / 5)
-timeLabel.place(relx=3 / 4, rely=2 / 5)
-timeVar.place(relx=0.9, rely=2 / 5)
-deploy1.place(relx=3 / 4, rely=3 / 5)
-deploy1Var.place(relx=0.9, rely=3 / 5)
-deploy2.place(relx=3 / 4, rely=4 / 5)
-deploy2Var.place(relx=0.9, rely=4 / 5)
+        self.hour = 0
+        self.minute = 0
+        self.second = 0
+        self.hTime = str(self.hour)
+        self.mTime = str(self.minute)
+        self.sTime = str(self.second)
+        self.strTime = ""
+        self.list = []
 
-# David
-col_width = 100
-tree.column("#0", anchor=tk.CENTER, width=col_width-50)
-for col in tree["columns"]:
-    tree.column(col, anchor=tk.CENTER, width=col_width)
+        self.data.place(relx=3 / 4, rely=0.05)
+        self.altitude.place(relx=3 / 4, rely=1 / 5)
+        self.altitudeVar.place(relx=0.9, rely=1 / 5)
+        self.timeLabel.place(relx=3 / 4, rely=2 / 5)
+        self.timeVar.place(relx=0.85, rely=2 / 5)
+        self.deploy1.place(relx=3 / 4, rely=3 / 5)
+        self.deploy1Var.place(relx=0.9, rely=3 / 5)
+        self.deploy2.place(relx=3 / 4, rely=4 / 5)
+        self.deploy2Var.place(relx=0.9, rely=4 / 5)
 
-scrollbar.config(command=tree.yview)
+        # David
+        col_width = 100
+        self.tree.column("#0", anchor=tk.CENTER, width=col_width-50)
+        for col in self.tree["columns"]:
+            self.tree.column(col, anchor=tk.CENTER, width=col_width)
 
-tree.grid(row=0, column=0, sticky=tk.W)
-scrollbar.grid(row=0, column=1, sticky=tk.N + tk.S)  # positions the scrollbar at the right (sticky = coordinates)
+        self.scrollbar.config(command=self.tree.yview)
 
+        self.tree.grid(row=0, column=0, sticky=tk.W)  # positions the scrollbar at the right (sticky = coordinates)
+        self.scrollbar.grid(row=0, column=1, sticky=tk.N + tk.S)
 
-def refresh():
-    # Carlos
-    altP.set(randint(0, 100))
-    d1.set(randint(0, 100))
-    d2.set(randint(0, 100))
-    master.after(1000, refresh)
-
-
-refresh()
-
-
-def clock():
-    # David
-    # elapsed_time = time.time() - start_time
-    #     # minutos = str(int(elapsed_time / 60))
-    #     # seg = str(int(elapsed_time % 60))
-
-    hour = dt.datetime.now().hour  # Obtains the current hour
-    minute = dt.datetime.now().minute  # Obtains the current minute
-    second = dt.datetime.now().second  # Obtains the current second
-    hTime = str(hour) + ":" + str(minute) + ":" + str(second)
-    # if int(seg) < 10:
-    #     num = minutos + " : 0" + seg
-    # else:
-    #     num = minutos + " : " + seg
-
-    tP.set(hTime)
-    master.after(1000, clock)
-
-
-clock()
-
-
-def table(list):
-    nums = [i for i in range(51)]  # List comprehension to obtain list of numbers/ can be changed to desired number
-    list = []  # Empty List created to store the actual time
-
-    for i in nums:
-        if i == 0:
-            pass
+    def real_time(self):
+        self.hour = dt.datetime.now().hour  # Obtains the current hour
+        self.minute = dt.datetime.now().minute  # Obtains the current minute
+        self.second = dt.datetime.now().second  # Obtains the current second
+        self.hTime = str(self.hour)
+        if self.minute < 10:
+            # Concatenate a 0 in front of number in case it is a single digit number
+            self.mTime = "0" + str(self.minute)
         else:
-            hour = dt.datetime.now().hour    # Obtains the current hour
-            minute = dt.datetime.now().minute   # Obtains the current minute
-            second = dt.datetime.now().second   # Obtains the current second
-            hTime = str(hour)
-            if minute < 10:
-                mTime = "0" + str(minute)    # Concatenate a 0 in front of number in case it is a single digit number
+            self.mTime = str(self.minute)
+        if self.second < 10:
+            self.sTime = "0" + str(self.second)  # If second is 5 it would show "05"
+        else:
+            self.sTime = str(self.second)
+        self.list = [(self.hTime, self.mTime, self.sTime)]  # Stores the actual time in time list
+        self.strTime = self.hTime + ":" + self.mTime + ":" + self.sTime
+
+    def refresh(self):
+        # Carlos
+        self.altP.set(randint(0, 100))
+        self.d1.set(randint(0, 100))
+        self.d2.set(randint(0, 100))
+        self.tP.set(self.strTime)
+        # self.master.after(1000, self.refresh)
+
+    def table(self):
+        nums = [i for i in range(51)]  # List comprehension to obtain list of numbers/ can be changed to desired number
+        for i in nums:
+            if i == 0:
+                pass
             else:
-                mTime = str(minute)
-            if second < 10:
-                sTime = "0" + str(second)   # If second is 5 it would show "05"
-            else:
-                sTime = str(second)
-            list = [(hTime, mTime, sTime)]  # Stores the actual time in time list
-            for hr, min, sec in list:
-                # Inserts the number of data and current time to tree
-                tree.insert("", i, text=str(i), values=(hr + ":" + min + ":" + sec))
-            list = []  # Resets the time list
-            # tree.update()    # Updates the tree every time it loops
-        master.after(1000, tree.update())
+                for hr, minute, sec in self.list:
+                    # Inserts the number of data and current time to tree
+                    self.tree.insert("", i, text=str(i), values=(hr + ":" + minute + ":" + sec))
+                self.list = []  # Resets the time list
+                self.real_time()
+            self.master.after(1000, self.tree.update(), self.refresh())
+
+    def handle_click(self, event):         # Function to prevent resize on the headings
+        if self.tree.identify_region(event.x, event.y) == "separator":
+            return "break"
 
 
-def handle_click(event):         # Function to prevent resize on the headings
-    if tree.identify_region(event.x, event.y) == "separator":
-        return "break"
-
-
-tree.bind('<Button-1>', handle_click)
-
-tList = []
-table(tList)
-master.mainloop()
+root = Draft1(tk.Tk())
+root.real_time()
+root.refresh()
+root.table()
+root.tree.bind('<Button-1>', root.handle_click)
+tk.mainloop()
